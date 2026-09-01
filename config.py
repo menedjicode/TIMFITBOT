0c33483c61de728cf8f26034df2862eae4b113e3
menedjicode <mandzz080808@gmail.com>
2026-09-01 09:18:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+import os
+
+ADMIN_IDS = os.getenv('ADMIN_IDS', '')
+if ADMIN_IDS:
+    ADMIN_IDS = [int(id.strip()) for id in ADMIN_IDS.split(',')]
+else:
+    ADMIN_IDS = []  # 👈 Если пусто — пустой список
 BOT_TOKEN=''
-ADM_IDS = []
