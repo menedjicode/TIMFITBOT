6e6536b674a0e278b2170ed4474bf8cf16880dc5
menedjicode <mandzz080808@gmail.com>
2026-09-01 10:02:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ if ADMIN_IDS:
     ADMIN_IDS = [int(id.strip()) for id in ADMIN_IDS.split(',')]
 else:
     ADMIN_IDS = []  # 👈 Если пусто — пустой список
-
+PROXY_URL = os.getenv('PROXY_URL', '')
 BOT_TOKEN = os.getenv('BOT_TOKEN', '')
