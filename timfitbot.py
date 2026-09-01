599fda1044e436c51fb60d6a6b13406cd6c9c58e
menedjicode <mandzz080808@gmail.com>
2026-09-01 09:47:01 +0300

    Update timfitbot.py

--- timfitbot.py
+++ timfitbot.py
@@ -30,7 +30,7 @@ ITEMS_PER_PAGE = 3  # Сколько тренеров на странице
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 import requests
-bot.apihelper.proxy = {
+apihelper.proxy = {
     'https': 'socks5://zapret:zapret@proxy.zapret.info:1194'
 }
 user_data = {}
